5393dbbdcd66af3b611beb213ee73b6fff0cec71
stefanluks <stefan.luks@rede.ulbra.br>
2020-08-04 19:16:30 -0300

    app pronto

--- wanted/settings.py
+++ wanted/settings.py
@@ -1,7 +1,7 @@
 
 import os
 
-#import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -116,4 +116,4 @@ LOGOUT_REDIRECT_URL='login'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
